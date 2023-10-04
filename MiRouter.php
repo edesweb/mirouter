b5cd8655913dc51efa36734b837a7be0401a8950
ddanone <raul.diaz@sonosuite.com>
2023-10-04 07:23:47 +0200

    Bugs fixed

--- MiRouter.php
+++ MiRouter.php
@@ -3,7 +3,7 @@
  * MiRouter Class to route http request to our configured PHP files.
  *
  * @author Raúl Díaz <raul.diaztorres@gmail.com>
- * @license MIT
+ * @license  MIT
  *
  * @link     https://github.com/edesweb/mirouter
  */
@@ -14,6 +14,7 @@ class MiRouter
     const DBGOUT = 2;
 
     // What To Debug
+    //private int $WTD=self::DBGNONE;
     private int $WTD=self::DBGOUT;
 
     private string $logFile;
@@ -24,20 +25,27 @@ class MiRouter
 
 
     //private array $routerIni;
-	public string $reason='';
-	public int $returnCode=self::RETCODE_OK;
-	public string $filename='';
+    public string $reason='';
+    public int $returnCode=self::RETCODE_OK;
+    public string $filename='';
+
     public array $libs_to_include=array();
-    private $plugin=false;
+
+    public $plugin=false;
 
     private function noRoute($reason): void
+    {
+        $this->returnCode = self::RETCODE_ERROR;
+        $this->reason 	  = $reason;
+    }
+    /*
+	private function route($filename): void
 	{
-		$this->returnCode = self::RETCODE_ERROR;
-		$this->reason 	  = $reason;
+		$this->returnCode = self::RETCODE_OK;
+		$this->filename	  = $filename;
 	}
-
-    private function dbg( $msg ): void
-	{
+    */
+    private function dbg( $msg ){
         if( $this->WTD&self::DBGOUT ){
             echo $msg."\n";
         }
@@ -46,13 +54,17 @@ class MiRouter
         }
     }
 
-	/**
-	 * @param array $routerIni - array with the .ini main config
-	 * @param int $wtd - what to debug
-	 *
-	 */
-    public function __construct( array $routerIni, int $wtd=null )
-	{
+    /**
+     * @param RequestUri $requestUri
+     * @param $paths
+     *
+     * Sample: pepe.acme.com/xxx
+     */
+    //public function __construct( RequestUri $requestUri, array $routerIni)
+    public function __construct( array $routerIni, int $wtd=null ) //self::DBGNONE )
+    {
+
+        //$this->routerIni = $routerIni;
         $requestUri      = new RequestUri();
         $paths           = $routerIni['paths'];
 
@@ -67,71 +79,84 @@ class MiRouter
             }
         }
 
+
         if($this->WTD!=self::DBGNONE) $this->dbg( $this->eEcho($requestUri) );
 
-		// Parse hosts.ini
-    		$hosts = parse_ini_file($paths['routes'] . 'hosts.ini', true);
-            if($this->WTD!=self::DBGNONE) $this->dbg( $this->eEcho($hosts) );
+        // Parse hosts.ini
+        $hosts = parse_ini_file($paths['routes'] . 'hosts.ini', true);
+        if($this->WTD!=self::DBGNONE) $this->dbg( $this->eEcho($hosts) );
 
-		// Find the $requestUri->host or $requestUri->host_base section to get the routes value
-		// If $requestUri->host (john.acme.com) is not found then check for $requestUri->host_base (acme.com)
-            if (isset($hosts[$requestUri->host])) {
-                $hosts_section = $requestUri->host;
-            } else if (isset($hosts[$requestUri->host_base])) {
-                $hosts_section = $requestUri->host_base;
-            } else {
-                $this->noRoute("Host not declared: {$requestUri->host}");
+
+
+        // Find the $requestUri->host or $requestUri->host_base section to get the routes value
+        // If $requestUri->host (john.acme.com) is not found then check for $requestUri->host_base (acme.com)
+        if (isset($hosts[$requestUri->host])) {
+            $hosts_section = $requestUri->host;
+        } else if (isset($hosts[$requestUri->host_base])) {
+            $hosts_section = $requestUri->host_base;
+        } else {
+            $this->noRoute("Host not declared: {$requestUri->host}");
+            return;
+        }
+        if($this->WTD!=self::DBGNONE)  $this->dbg( "hosts.ini section: {$hosts_section}\n". $this->eEcho($hosts[$hosts_section]) );
+
+        // If routes value is false then no routes are defined
+        $routes = $hosts[$hosts_section]['routes'] ?? false;
+        if (!$routes) {
+            $this->noRoute("No routes defined for: {$hosts_section}");
+            return;
+        }
+        /*
+                // If uri==login then redirect to login
+                    if( strtolower($requestUri->uri)=='login' && $hosts[$hosts_section]['login']??false ){
+                        $this->route( $paths['src'] . $hosts[$hosts_section]['login'] );
+                        return;
+                    }
+
+                // If uri==logout then redirect to logout
+                    if( strtolower($requestUri->uri)=='logout' && $hosts[$hosts_section]['logout']??false ){
+                        $this->route( $paths['src'] . $hosts[$hosts_section]['logout'] );
+                        return;
+                    }
+        */
+        // If routes is true, then must exists a .ini filename with the section name
+        $routes_filename = $paths['routes'] . $hosts_section . '.ini';
+        if (!file_exists($routes_filename)) {
+            $this->noRoute("Can't find routes file: {$routes_filename}");
+            return;
+        }
+        if($this->WTD!=self::DBGNONE)  $this->dbg("routes_filename: {$routes_filename}" );
+
+        // Parse $routes_filename
+        $rini = parse_ini_file($routes_filename, true);
+        if($this->WTD!=self::DBGNONE)  $this->dbg('rini: '. $this->eEcho($rini)."\nTry to find section: {$requestUri->uri }");
+
+        // Get section for the requested uri
+        $uri_section = $rini[$requestUri->uri] ?? null;
+        if ($uri_section == null) {
+
+            // if wild_router then return the $requestUri->uri.php
+            $wild_router = $rini['mirouterconfig']['wild_router']?? false;
+            if( $wild_router ){
+                $script_to_load = $requestUri->uri. (( strpos($requestUri->uri,'.')===false )? '.php': '');
+                if( !file_exists($paths['src'].$script_to_load) ){
+                    $this->noRoute("Can't find route to {$script_to_load}");    //" in file {$routes_filename}");
+                    return;
+                }
+                $this->filename     = $paths['src'] .$script_to_load;
+                $this->returnCode   = self::RETCODE_OK;
                 return;
             }
-            if($this->WTD!=self::DBGNONE)  $this->dbg( "hosts.ini section: {$hosts_section}\n". $this->eEcho($hosts[$hosts_section]) );
-
-		// If routes value is false then no routes are defined
-            $routes = $hosts[$hosts_section]['routes'] ?? false;
-            if (!$routes) {
-                $this->noRoute("No routes defined for: {$hosts_section}");
-                return;
-            }
-
-		// If routes is true, then must exist a .ini filename with the section name
-            $routes_filename = $paths['routes'] . $hosts_section . '.ini';
-            if (!file_exists($routes_filename)) {
-                $this->noRoute("Can't find routes file: {$routes_filename}");
-                return;
-            }
-            if($this->WTD!=self::DBGNONE)  $this->dbg("routes_filename: {$routes_filename}" );
-
-		// Parse $routes_filename
-            $rini = parse_ini_file($routes_filename, true);
-            if($this->WTD!=self::DBGNONE)  $this->dbg('rini: '. $this->eEcho($rini)."\nTry to find section: {$requestUri->uri }");
-
-		// Get section for the requested uri
-            $uri_section = $rini[$requestUri->uri] ?? null;
+            // Check if exists [default] section, if not -> error
+            if($this->WTD!=self::DBGNONE)  $this->dbg("Not found, trying to find  [default] section...");
+            $requestUri->uri = 'default';
+            $uri_section     = $rini[$requestUri->uri] ?? null;
             if ($uri_section == null) {
-
-                // if wild_router then return the $requestUri->uri.php
-                    $wild_router = $rini['mirouterconfig']['wild_router']?? false;
-                    if( $wild_router ){
-                        $script_to_load = $requestUri->uri. (( strpos($requestUri->uri,'.')===false )? '.php': '');
-                        if( !file_exists($paths['src'].$script_to_load) ){
-                            $this->noRoute("Can't find route to {$script_to_load}");    //" in file {$routes_filename}");
-                            return;
-                        }
-                        $this->filename     = $paths['src'] .$script_to_load;
-                        $this->returnCode   = self::RETCODE_OK;
-                        return;
-                    }
-
-
-                // Check if exists [default] section, if not -> error
-                    if($this->WTD!=self::DBGNONE)  $this->dbg("Not found, trying to find  [default] section...");
-                    $requestUri->uri = 'default';
-                    $uri_section     = $rini[$requestUri->uri] ?? null;
-                    if ($uri_section == null) {
-                        $this->noRoute("Can't find route to {$requestUri->uri} in file {$routes_filename}");
-                        return;
-                    }
+                $this->noRoute("Can't find route to {$requestUri->uri} in file {$routes_filename}");
+                return;
             }
-            if($this->WTD!=self::DBGNONE)  $this->dbg("Section: {$requestUri->uri}\n" . $this->eEcho($uri_section) );
+        }
+        if($this->WTD!=self::DBGNONE)  $this->dbg("Section: {$requestUri->uri}\n" . $this->eEcho($uri_section) );
 
         /**
          * if $uri_section[public]==false then authentication required
@@ -156,66 +181,72 @@ class MiRouter
          * auth-req | false       | NO
          * */
 
-            $uri_section['public']   = $uri_section['public']?? true;
-            $uri_section['auth-req'] = $uri_section['auth-req']?? false;
+        $uri_section['public']   = $uri_section['public']?? true;
+        $uri_section['auth-req'] = $uri_section['auth-req']?? false;
 
-            if ( $uri_section['public']==false || $uri_section['auth-req']==true ) {
-                if($this->WTD!=self::DBGNONE)  $this->dbg("Authentication required ({$requestUri->uri}['public']=false or {$requestUri->uri}['auth-req']=true )");
+        if ( $uri_section['public']==false || $uri_section['auth-req']==true ) {
+            if($this->WTD!=self::DBGNONE)  $this->dbg("Authentication required ({$requestUri->uri}['public']=false or {$requestUri->uri}['auth-req']=true )");
 
-                // Check if session_checker key exists in the routes_file
-                if( isset( $rini['mirouterconfig']['session_checker'] ) ){
-                    $auth_checker_file = $paths['src'] . $rini['mirouterconfig']['session_checker'];
-                }else{
-                    // Try to get the session_checker key in the hosts file
-                    if (!isset($hosts[$hosts_section]['session_checker'])) {
-                        $this->noRoute("check-login not defined in hosts.ini, section {$hosts_section}");
-                        return;
-                    }
-                    $auth_checker_file = $paths['src'] . $hosts[$hosts_section]['session_checker'];
-                }
-
-                if( !file_exists($auth_checker_file) ){
-                    $this->noRoute("session_checker file does not exists: {$auth_checker_file}");
+            // Check if session_checker key exists in the routes_file
+            if( isset( $rini['mirouterconfig']['session_checker'] ) ){
+                $auth_checker_file = $paths['src'] . $rini['mirouterconfig']['session_checker'];
+            }else{
+                // Try to get the session_checker key in the hosts file
+                if (!isset($hosts[$hosts_section]['session_checker'])) {
+                    $this->noRoute("check-login not defined in hosts.ini, section {$hosts_section}");
                     return;
                 }
-
-                // include the $auth_checker_file and try to execute a function called like the file but without '.php'
-                // that function is the responsible of checking if the user is authenticated.
-                include $auth_checker_file;
-                $function_to_execute = strtolower( str_replace('.','_',str_ireplace('.php','',basename($auth_checker_file)) ) );
-                if( !function_exists($function_to_execute)){
-                    $this->noRoute("[{$function_to_execute}] function does not exists in: {$auth_checker_file}");
-                    return;
-                }
-                $checkSessionRet = $function_to_execute();
-                if( $checkSessionRet->returnCode != self::RETCODE_OK ){
-                    $this->returnCode 	= $checkSessionRet->returnCode;
-                    $this->reason		= $checkSessionRet->returnReason; //'Authentication required';
-                    //$this->filename		= ( $hosts[$hosts_section]['login'] )?? '/login';
-                    return;
-                }
+                $auth_checker_file = $paths['src'] . $hosts[$hosts_section]['session_checker'];
             }
 
+            if( !file_exists($auth_checker_file) ){
+                $this->noRoute("session_checker file does not exists: {$auth_checker_file}");
+                return;
+            }
 
+            // include the $auth_checker_file and try to execute a function called like the file but without '.php'
+            // that function is the responsible of checking if the user is authenticated.
+            require $auth_checker_file;
+            $function_to_execute = strtolower( str_replace('.','_',str_ireplace('.php','',basename($auth_checker_file)) ) );
+            if( !function_exists($function_to_execute)){
+                $this->noRoute("[{$function_to_execute}] function does not exists in: {$auth_checker_file}");
+                return;
+            }
+            $checkSessionRet = $function_to_execute();
+            if( $checkSessionRet->returnCode != self::RETCODE_OK ){
+                $this->returnCode 	= $checkSessionRet->returnCode;
+                $this->reason		= $checkSessionRet->returnReason; //'Authentication required';
+                return;
+            }
+        }
 
-        // if $uri_section[script] is empty load the section_name.php
-            $this->plugin           = false;
-            $uri_section['script']  = $uri_section['script']?? null;
-            if ($uri_section['script'] == null) {
-                $script_to_load = $paths['src'] . $requestUri->auri[count($requestUri->auri) - 1] . '.php';
-                if($this->WTD!=self::DBGNONE)  $this->dbg("Load(1) {$script_to_load}");
+        // Check script
+        $this->plugin           = false;
+        $uri_section['script']  = $uri_section['script']?? null;
+        if ($uri_section['script'] == null) {
+            // if $uri_section[script] is empty load the section_name.php
+            $script_to_load = $paths['src'] . $requestUri->auri[count($requestUri->auri) - 1] . '.php';
+            if($this->WTD!=self::DBGNONE)  $this->dbg("Load(1) {$script_to_load}");
+            if (!file_exists($script_to_load)) {
+                $this->noRoute("Can't load {$script_to_load}");
+                return;
+            }
+        } else {
+            $script_to_load = $paths['src'] . $uri_section['script'];
+            if($this->WTD!=self::DBGNONE)  $this->dbg("Load(2) {$script_to_load}");
+
+            $this->filename     = $script_to_load;
+
+            // Check if are plugins defined
+            $re = ( $routerIni['plugins']['regexp'] )?? false;
+            if( !$re ){
                 if (!file_exists($script_to_load)) {
                     $this->noRoute("Can't load {$script_to_load}");
                     return;
                 }
-            } else {
-                $script_to_load = $paths['src'] . $uri_section['script'];
-                if($this->WTD!=self::DBGNONE)  $this->dbg("Load(2) {$script_to_load}");
-
-
-                $this->filename     = $script_to_load;
-                $re = '/'.$routerIni['plugins']['regexp'].'/';
-
+            }else{
+                // Check if script requires a plugin
+                $re = '/'.$re.'/';
                 if( preg_match_all($re, $uri_section['script'], $matches, PREG_SET_ORDER, 0) ){
                     //echo $matches[0]['PLUGIN'].'<BR>'.$matches[0]['SCRIPT'];exit;
                     $plugin = $matches[0]['PLUGIN']?? false;
@@ -231,25 +262,26 @@ class MiRouter
                     }
                 }
             }
+        }
+
 
 
         // Replace the lib key by the mirouterconfig/libs section keys
-            $uri_section['libs'] = $uri_section['libs']?? '';
-            $libs = trim($uri_section['libs']);
-            $libs2include = array();
-            if($libs!=''){
-                $libs = explode(',',$libs);
-                foreach($libs as $lib){
-                    if( isset( $rini['mirouterconfig/libs'][trim($lib)]) ){
-                        $this->libs_to_include[] = $rini['mirouterconfig/libs'][trim($lib)];
-                    }
+        $uri_section['libs'] = $uri_section['libs']?? '';
+        $libs = trim($uri_section['libs']);
+        $libs2include = array();
+        if($libs!=''){
+            $libs = explode(',',$libs);
+            foreach($libs as $lib){
+                if( isset( $rini['mirouterconfig/libs'][trim($lib)]) ){
+                    $this->libs_to_include[] = $rini['mirouterconfig/libs'][trim($lib)];
                 }
             }
+        }
 
 
         $this->returnCode   = self::RETCODE_OK;
-
-	}
+    }
 
     private function eEcho($s,$ret=true): string
     {
@@ -269,17 +301,17 @@ class MiRouter
 class RequestUri
 {
     // $host contains _server['HTTP_HOST']
-    public string|array $host;
+    public  $host;
     // if $host has subdomains contains base domain
-    public string|array $host_base;
+    public  $host_base;
     // uri contains the uri without parameters
-    public string|array|null $uri;
+    public  $uri;
     // auri is an index of $uri folders
-    public array $auri;
+    public  $auri;
     // params is an associative array with the _GET params
-    public string|array $params;
+    public  $params;
     // _GET params as string
-    public string|null $paramsString=null;
+    public  $paramsString=null;
     // contains _SERVER['REQUEST_METHOD'] which verb is used on the call
     public string $method;
     public function __construct()
@@ -303,11 +335,17 @@ class RequestUri
             $params 	= explode('&', trim($params));
             foreach ($params as $k => $v) {
                 $v = explode('=', $v);
-                $this->params[$v[0]]=$v[1]??'';
+                $this->params[$v[0]]=$v[1];
             }
         }
+
         $this->uri = preg_replace('/\/\/{1,}/', '/', $this->uri);		// Replace multiple //
+
         $this->uri = preg_replace('/(^\/)|(\/$)/', '', $this->uri);	// Replace first /
+        //$this->uri = preg_replace('/^\//', '', $this->uri);	// Replace first /
+        //$this->uri = preg_replace('/\/$/', '', $this->uri);	// Replace first and last /
+
         $this->auri=explode('/',$this->uri);
+        //$this->uri .= '/';
     }
 }
